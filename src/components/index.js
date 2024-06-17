090ece26375b4086ac4b2e18d3d9054ed35491aa
Naziur Rahman Khan (Aiman) <naziurrahmankhan101@gmail.com>
2024-06-17 20:40:49 +0600

    Upto TopBar Done

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import TextInput from "./TextInput";
 import Loading from "./Loading";
 import CustomButton from "./CustomButton";
-export {Loading, TextInput , CustomButton};
+import TopBar from "./TopBar";
+
+export {Loading, TextInput , CustomButton, TopBar};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import TopBar from '../components/TopBar';
 
 const Home = () => {
+  const {user} = useSelector((state)=>state.user);
   return (
-    <div>Home</div>
+    <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgColor
+    lg:rounded-lg h-screen overflow-hidden'>
+      <TopBar />
+      <div className='w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full'>
+        {/*LEFT*/}
+        <div className='hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6
+        overflow-y-auto'></div>
+        {/*CENTER*/}
+        <div></div>
+        {/*RIGHT*/}
+        <div></div>
+
+      </div>
+    </div>
   )
 }
 
-export default Home
+export default Home;
