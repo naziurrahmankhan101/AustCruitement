038027dc554288acf3ffe2b48990bea1a29bf144
Marjana026 <marjana.salauddin@gmail.com>
2024-06-18 01:46:24 +0600

    added profile card

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import TextInput from "./TextInput";
 import Loading from "./Loading";
 import CustomButton from "./CustomButton";
 import TopBar from "./TopBar";
+import ProfileCard from "./ProfileCard";
 
-export {Loading, TextInput , CustomButton, TopBar};
+export {Loading, TextInput , CustomButton, TopBar, ProfileCard};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import TopBar from '../components/TopBar';
+import ProfileCard from '../components/ProfileCard';
 
 const Home = () => {
   const {user} = useSelector((state)=>state.user);
@@ -11,7 +12,9 @@ const Home = () => {
       <div className='w-full flex gap-2 lg:gap-4 pt-5 pb-10 h-full'>
         {/*LEFT*/}
         <div className='hidden w-1/3 lg:w-1/4 h-full md:flex flex-col gap-6
-        overflow-y-auto'></div>
+        overflow-y-auto'>
+          <ProfileCard user={user} />
+        </div>
         {/*CENTER*/}
         <div></div>
         {/*RIGHT*/}
